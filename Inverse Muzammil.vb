b082da0520cafe1aab7d157120cfc0ad17f9aff2
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2020-11-09 22:58:17 -0800

    Inverse

---
+++ Inverse Muzammil.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim X, Mychar As String
+        Dim a As Integer
+        X = ""
+        Mychar = ""
+        a = 0
+
+        Console.Write("PLease type: ")
+        X = Console.ReadLine
+        For a = Len(X) To 1 Step (-1)
+            Mychar = Mid(X, a, 1)
+            Console.Write(Mychar)
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
