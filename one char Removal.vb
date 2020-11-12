c27ee8a91f8736c272557c8cb6e4f2516ed6f1d7
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2020-11-11 23:28:00 -0800

    Add files via upload

---
+++ one char Removal.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, name As String
+        Dim mychar, remove As Char
+        Dim L1, X As Integer
+        str1 = ""
+        name = ""
+        mychar = ""
+        L1 = 0
+        X = 0
+        Console.Write("Enter our stuff: ")
+        str1 = Console.ReadLine
+        Console.Write("What do you want to Remove: ")
+        remove = Console.ReadLine()
+        L1 = Len(str1)
+        For X = 1 To L1
+            mychar = Mid(str1, X, 1)
+            If mychar <> remove Then
+                name = name & mychar
+            End If
+        Next
+        Console.WriteLine("Name: " & name)
+        Console.ReadKey()
+    End Sub
+
+End Module
