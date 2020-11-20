8b2825f44564c1c3b19f96937e274c66e4e9f00f
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2020-11-20 22:21:22 +0500

    Update and rename 2 Removal.vb to 2 Replacing.vb

--- Project/2 Replacing.vb
+++ Project/2 Replacing.vb
@@ -1,27 +1,36 @@
-﻿Module Module1
+Module Module1
 
     Sub Main()
         'Declaration and Initialization
         Dim Str1 As String = ""
         Dim ostr As String = ""
         Dim rov As Char = ""
-        Dim schar As Char = ""
+        Dim cha As Char = ""
         Dim x As Integer = 0
+        Dim rep As Char = ""
         'Input Phrase and the Character you wnt to Remove
         Console.Write("Type in your Phrase: ")
         Str1 = Console.ReadLine()
+        Str1 = LCase(Str1)
         Console.Write("What do you want to Remove: ")
         rov = Console.ReadLine
-        'Removing the Character no matter if it is in Uppercase or Lowercase
+        rov = LCase(rov)
+        Console.Write("What do you want to replace it with:")
+        rep = Console.ReadLine
+        rep = LCase(rep)
+        'Replacing the Character no matter if it is in Uppercase or Lowercase
         For x = 1 To Len(Str1)
-            schar = Mid(Str1, x)
-            If schar <> LCase(rov) Or schar <> UCase(rov) Then
-                ostr = ostr & schar
+            cha = Mid(Str1, x, 1)
+            If cha = rov Then
+                ostr = ostr & rep
+            Else
+                ostr = ostr & cha
             End If
         Next
         'OutPut
         Console.Write("YOUR RESULT: " & ostr)
         Console.ReadKey()
+
     End Sub
 
 End Module
