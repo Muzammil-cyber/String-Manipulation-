d3144c93d86bb7b605d48dd501d14e9ac7fcda47
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2020-12-20 20:58:05 +0500

    Add files via upload

---
+++ Run length coding.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1 As String = ""
+        Dim cha As Char = ""
+        Dim y As Integer = 1
+        Dim count As Integer = 0
+        Console.Write("Enter your Text: ")
+        str1 = Console.ReadLine
+        Do
+            cha = Mid(str1, y, 1)
+            count = 0
+            Do
+                If cha = Mid(str1, y, 1) Then
+                    count = count + 1
+                    y = y + 1
+                End If
+            Loop Until (cha <> Mid(str1, y, 1))
+            Console.Write(cha & " " & count & " ")
+        Loop Until (y >= Len(str1))
+        Console.ReadKey()
+    End Sub
+
+End Module
